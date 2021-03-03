9db85e2f26f2ba30964fe752aee1f9854fb492d0
[Andrew Core] <[adcore89@gmail.com]>
2021-03-03 17:16:20 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,7 +25,7 @@ var startGame = function() {
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
 
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
@@ -35,9 +35,9 @@ var startGame = function() {
       break;
     }
   }
-
   // after loop ends, we are either out of playerHealth or enemies to fight, so run the endGame function
   endGame();
+  // function to generate a randon numeric value 
 };
 
 // function to end the entire game
@@ -59,6 +59,7 @@ var endGame = function() {
   } else {
     window.alert('Thank you for playing Battlebots! Come back soon!');
   }
+  // function to generate a randon numeric value 
 };
 
 // fight function (now with parameter for enemy's name)
@@ -76,17 +77,19 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         shop();
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
-    console.log(
-      playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
-    );
+    var damage = randomNumber(playerAttack -3, playerAttack);
+    
+    enemyHealth = Math.max(0, enemyHealth - damage);
+   
+    playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
+    
 
     // check enemy's health
     if (enemyHealth <= 0) {
@@ -102,7 +105,6 @@ var fight = function(enemyName) {
       if (storeConfirm) {
         shop();
       }
-
       // leave while() loop since enemy is dead
       break;
     } else {
@@ -110,10 +112,12 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
-    console.log(
+    var damage = randomNumber(enemyAttack -3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
+    
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
-    );
+    
 
     // check player's health
     if (playerHealth <= 0) {
@@ -122,8 +126,6 @@ var fight = function(enemyName) {
       break;
     } else {
       window.alert(playerName + ' still has ' + playerHealth + ' health left.');
-    }
-  }
 };
 
 // go to shop between battles function
@@ -132,7 +134,6 @@ var shop = function() {
   var shopOptionPrompt = window.prompt(
     'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice.'
   );
-
   // use switch case to carry out action
   switch (shopOptionPrompt) {
     case 'refill':
@@ -156,7 +157,14 @@ var shop = function() {
       shop();
       break;
   }
-};
+} 
+  }
+  };
+// function to generate a randon numeric value 
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
 
+return value;   
+};  
 // start first game when page loads
-startGame();
+startGame(); 
