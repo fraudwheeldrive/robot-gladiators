6669c1c9181723a58f3798a3bc95dd2a618b08f2
[Andrew Core] <[adcore89@gmail.com]>
2021-03-05 20:39:34 -0500

    bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,6 +11,7 @@ var startGame = function() {
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+      debugger;
 
       // pick new enemy to fight based on the index of the enemyInfo array
       var pickedEnemyObj = enemyInfo[i];
@@ -173,9 +174,22 @@ var randomNumber = function(min, max) {
 };
 /* END GAME FUNCTIONS */
 
-/* GAME INFORMATION / VARIABLES */
+/*G AME INFORMATION / VARIABLES */
 
 // player information
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("what is your robots name?");
+  }
+
+console.log("Your robots name is " + name);  
+return name;
+};
+var playerInfo = {
+  name: getPlayerName(),
+}
 var playerInfo = {
   name: window.prompt("What is your robot's name?"),
   health: 100,
